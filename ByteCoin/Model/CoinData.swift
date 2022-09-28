4eff1f24e0498f7585cceab34ea715fef6eadd31
lurdharry <azeezhammed57@gmail.com>
2022-09-29 00:32:56 +0100

    chore: add model

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by lurdharry on 9/26/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable {
+    let rate :Double
+}
